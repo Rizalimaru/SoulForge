c3052256c1abd7ae535d28ae71a40b933a04e4dd
Rizalimaru <113398108+Rizalimaru@users.noreply.github.com>
2025-04-12 11:04:44 +0700

    Add Buff Data

---
+++ Assets/ScriptableObjectData/BuffData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBuff", menuName = "Buff System/Buff Data")]
+public class BuffData : ScriptableObject
+{
+    public string buffName;
+    public string description;
+    public Sprite icon;
+    public BuffType buffType;
+    public int value;
+}
+
+public enum BuffType
+{
+    Attack,
+    Defense,
+    Speed,
+    Healing,
+    HP,
+    pickRadius,
+    
+
+}
